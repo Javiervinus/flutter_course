dc0812bd0b31f98522563ab2add2842bb75f423c
javier <javiervin@hotmail.com>
2022-01-17 05:37:59 -0500

    cambios

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -3,10 +3,11 @@ import 'package:platzi_course/screens/home/components/header/gradiant_back.dart'
 import 'package:platzi_course/screens/profile/components/header.dart';
 import 'package:platzi_course/screens/profile/components/place.dart';
 import 'package:platzi_course/shared/container_app.dart';
+import 'package:fading_edge_scrollview/fading_edge_scrollview.dart';
 
 class ProfileTrips extends StatelessWidget {
   ProfileTrips({Key? key}) : super(key: key);
-
+  final _scrollController = ScrollController();
   Container exp() {
     return Container(
       color: Colors.black,
@@ -20,27 +21,33 @@ class ProfileTrips extends StatelessWidget {
       body: Stack(children: [
         const ProfileHeader(),
         Padding(
-          padding: const EdgeInsets.only(top: 280, left: 20, right: 20),
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              Place(
-                pathImage: "assets/images/mountain_stars.jpeg",
-                placeName: "Estrellas",
+          padding: const EdgeInsets.only(top: 270, left: 20, right: 20),
+          child: Container(
+            child: FadingEdgeScrollView.fromScrollView(
+              child: ListView(
+                controller: _scrollController,
+                padding: const EdgeInsets.only(top: 20),
+                children: [
+                  Place(
+                    pathImage: "assets/images/mountain_stars.jpeg",
+                    placeName: "Estrellas",
+                  ),
+                  Place(
+                    pathImage: "assets/images/beach_palm.jpeg",
+                    placeName: "Rio de Janeiro",
+                  ),
+                  Place(
+                      pathImage: "assets/images/mountain.jpeg",
+                      placeName: "Montaña"),
+                  Place(
+                      pathImage: "assets/images/river.jpeg",
+                      placeName: "Rio Luna"),
+                  Place(
+                      pathImage: "assets/images/sunset.jpeg",
+                      placeName: "Atardecer"),
+                ],
               ),
-              Place(
-                pathImage: "assets/images/beach_palm.jpeg",
-                placeName: "Rio de Janeiro",
-              ),
-              Place(
-                  pathImage: "assets/images/mountain.jpeg",
-                  placeName: "Montaña"),
-              Place(
-                  pathImage: "assets/images/river.jpeg", placeName: "Rio Luna"),
-              Place(
-                  pathImage: "assets/images/sunset.jpeg",
-                  placeName: "Atardecer"),
-            ],
+            ),
           ),
         )
       ]),
